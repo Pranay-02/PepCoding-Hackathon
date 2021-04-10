178994d86a7b000cbc93c0141c0e1abbd9f776a5
Pranay Singhal <66167464+Pranay-02@users.noreply.github.com>
2021-04-11 00:56:34 +0530

    Delete temp2.js

--- temp2.js
+++
@@ -1,3 +0,0 @@
-module.exports = "Hello There. \nThis is an automated email generator. This was just a testing email. \nPlease Ignore the mail and Do not reply to it. \n\nThanks and Regards";
-
-    
